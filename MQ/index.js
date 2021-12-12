c4805e8033819932b80869e4c675dad962dd584e
Frederik <Frederiklundbeck@live.dk>
2021-12-12 13:24:52 +0100

    Message queue done

---
+++ MQ/index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+
+const db = require("./queue");
+
+// xml2js config
+const xml2js = require("xml2js");
+const xmlBuilder = new xml2js.Builder();
+
+// express config
+app.use(express.json());
+
+app.get("/subscribe/:subscriberId", (req, res) => {
+    const subscriberId = req.params.subscriberId;
+    let messages = []; 
+    
+    // Check if format query parameter is set, else set it to json
+    const format = req.query.format ?? "json";
+
+    // Try to subscribe to the subscriber id
+    try {
+        messages = db.subscribe(subscriberId);
+    } catch (error) {
+        res.status(500).json({error: "something broke internally!"})
+    }
+
+    switch (format.toLocaleLowerCase()) {
+        case "json":
+            res.json(messages);
+            break;
+        case "xml":
+            res.set("Content-type", "text/xml");
+            
+            for (let i = 0; i < messages.length; i++) {
+                messages[i] = { messageObj: messages[i] };
+            }
+
+            res.send(xmlBuilder.buildObject({ messages }));
+            break;
+        default:
+            res.status(404).json({error: "format type is not supported!"});
+            break;
+    }
+
+});
+
+app.post("/publish", (req, res) => {
+
+    const { publisherId, subscriberId, message } = req.body;
+
+    if (!publisherId || !subscriberId || !message) {
+        res.status(400).json({error: "your json body was not correct!"});
+    }
+    else {
+        try {
+            db.publish(publisherId, subscriberId, message);
+            res.json({message: "your message was successfully published!"});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({error: "something broke internally!"});
+        }
+    }
+});
+
+// Start server
+app.listen(8080, err => {
+    if (err) {
+        console.log("Server cannot listen...");
+        return;
+    }
+    console.log("Server listening on port", 8080);
+});
---
+++ MQ/queue.js
@@ -0,0 +1,29 @@
+const { JsonDB } = require("node-json-db");
+const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
+const uuid = require("uuid");
+
+let db = new JsonDB(new Config("db.json", true, true, "/"));
+
+// Publish new message to queue.
+function publish(publisherId, subscriberId, message) {
+    const id = uuid.v4();
+    db.push("/messages[]", 
+    {id: id, publisherId: publisherId, subscriberId: subscriberId, message: message}, 
+    true);
+}
+
+// Find and return all available messages from queue.
+function subscribe(subscriberId) {
+    const messages = db.getData("/messages").filter(m => m.subscriberId == subscriberId);
+    
+    messages.forEach(message => {
+        db.delete("/messages[" + db.getIndex("/messages", message.id) + "]");
+    });
+
+    return messages;
+}
+
+module.exports = {
+    publish: publish,
+    subscribe: subscribe
+}
